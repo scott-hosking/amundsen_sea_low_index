6d0d244e93248ec3bdd24063d117b11f5e486176
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-07-30 14:20:43 +0100

    fix loggers

--- src/asli/__init__.py
+++ src/asli/__init__.py
@@ -8,5 +8,4 @@ from . import data, plot, utils
 from .params import CALCULATION_VERSION, ASL_REGION, SOFTWARE_VERSION
 
 import os, logging
-logging.getLogger("asli").addHandler(logging.NullHandler())
 logging.basicConfig(level=os.environ.get('ASLI_LOGLEVEL', 'INFO').upper())
